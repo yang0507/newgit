ed89fab08761c1fbac2d90538c93703bbc40ca23
yang0507 <18220095376@163.com>
2018-06-09 11:23:28 +0800

    码云测试

---
+++ JavaTest/src/first.java
@@ -0,0 +1,16 @@
+/**
+ * @author yangzhao
+ * @date 2018/6/9 10:11
+ */
+public class first {
+
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
---
+++ src/second.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author yangzhao
+ * @date 2018/6/9 10:03
+ */
+public class second {
+}
